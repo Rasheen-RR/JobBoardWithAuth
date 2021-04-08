98dc56c7e02dec7286e6a5793b959aff9d56e889
Rasheen <A00229247@mycambrian.ca>
2021-04-08 16:43:20 -0400

    Added caching

--- JobBoardWithAuth/wwwroot/service-worker.js
+++ JobBoardWithAuth/wwwroot/service-worker.js
@@ -1,4 +1,35 @@
-﻿// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+﻿function swInstall(event) {
+    console.info("installing")
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                './',
+                './css/site.css',
+                './js/site.js',
+                './favicon.ico',
+                './js/site.js?v=4q1jwFhaPaZgr8WAUSrux6hAuh0XDg9kPS3xIVq36I0',
+                './lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+                './lib/jquery/dist/jquery.min.js',
+                './lib/bootstrap/dist/css/bootstrap.min.css ',
+                './JobApplications',
+                './Candidates',
+                './Educations',
+                './Employers',
+                './Experiences',
+                './Resumes',
+                './JobPostings',
+                './Home'
+            ])
+        })
+    )
+}
+
+self.addEventListener('install', swInstall);
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cacheResponse) {
+            return cacheResponse
+        })
+    )
+})
