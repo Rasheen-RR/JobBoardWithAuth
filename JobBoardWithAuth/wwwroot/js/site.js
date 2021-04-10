9f3f62f859e00d52e06b22f333d4926246271558
Rasheen <A00229247@mycambrian.ca>
2021-04-10 02:36:41 -0400

    Added caching for job posts

--- JobBoardWithAuth/wwwroot/js/site.js
+++ JobBoardWithAuth/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function cache() {
+    let isCached = false
+    offlineBtn.on('click', async () => {
+        let cache = await caches.open('offline-jobs').then((cache) => {
+            isCached = await cache.match(window.location.href)
+
+            let cacheItem = () => {
+                cache.add(window.location.href)
+            }
+
+            if (cached) {
+                offlineBtn.val('Remove from cache')
+
+                cacheItem = () => {
+                    cache.delete(windows.location.href)
+                }
+            } else {
+                offlineBtn.val('Add to cache')
+            }
+        })
+
+        cacheItem();
+    })
+}
